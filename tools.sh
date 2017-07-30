8af68aef7271cb263e4fdc36b18e4b4495aae833
Henrik Jonsson <me@hkjn.me>
2017-07-30 17:57:37 +0000

    Remove broken args check

--- tools.sh
+++ tools.sh
@@ -7,10 +7,6 @@ LOG_PREFIX="$(basename $0)"
 
 # exists returns 0 if the given command exists on PATH.
 exists() {
-  [[ $? -ne 1 ]] && {
-    echo "Usage: exists some-tool" >&2
-    return 1
-  }
   if which $1 1>/dev/null; then
     return 0
   fi
